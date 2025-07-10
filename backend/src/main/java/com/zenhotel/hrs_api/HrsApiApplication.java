0e2884e9417d0705b2499735daf2bc46eb8f368a
katgithb <90700219+katgithb@users.noreply.github.com>
2025-07-10 05:33:23 +0530

    Initial commit - Scaffolding backend skeleton

---
+++ backend/src/main/java/com/zenhotel/hrs_api/HrsApiApplication.java
@@ -0,0 +1,13 @@
+package com.zenhotel.hrs_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsApiApplication.class, args);
+	}
+
+}
