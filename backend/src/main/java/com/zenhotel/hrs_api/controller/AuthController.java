40eecb3cdada3a2f562d45818692650fc04a66f9
katgithb <90700219+katgithb@users.noreply.github.com>
2025-07-23 17:52:41 +0530

    Implement AuthController for user authentication and registration
    - Add /signup endpoint to handle user registration
    - Add /login endpoint to handle user authentication
    - Include necessary imports for APIs, requests, and services

---
+++ backend/src/main/java/com/zenhotel/hrs_api/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.zenhotel.hrs_api.controller;
+
+import com.zenhotel.hrs_api.payload.ApiResponse;
+import com.zenhotel.hrs_api.payload.UserAuthRequest;
+import com.zenhotel.hrs_api.payload.UserAuthResponse;
+import com.zenhotel.hrs_api.payload.UserRegistrationRequest;
+import com.zenhotel.hrs_api.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse> registerUser(
+            @RequestBody @Valid UserRegistrationRequest request) {
+
+        return new ResponseEntity<>(userService.registerUser(request),
+                HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserAuthResponse> loginUser(
+            @RequestBody @Valid UserAuthRequest request) {
+
+        return ResponseEntity.ok(userService.loginUser(request));
+    }
+
+}
